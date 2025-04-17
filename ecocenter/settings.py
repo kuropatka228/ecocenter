05cf497e0533d287b5e17c8d79e24287e34dc660
kuropatka228 <egorshumilo25@gmail.com>
2025-04-18 02:04:24 +0900

    Фикс путей static файлов3

--- ecocenter/settings.py
+++ ecocenter/settings.py
@@ -79,8 +79,8 @@ TIME_ZONE = 'Asia/Chita'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ecocenter', 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
