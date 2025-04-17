c357db9bb1e3827ba3039253c96e3d2236ba866d
kuropatka228 <egorshumilo25@gmail.com>
2025-04-18 01:27:54 +0900

    Фикс путей static файлов

--- ecocenter/settings.py
+++ ecocenter/settings.py
@@ -80,7 +80,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ecocenter', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
